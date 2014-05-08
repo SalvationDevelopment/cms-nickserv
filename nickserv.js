f6c970bd9165858e9688c9992a5e6e5973784c0f
Jamezs Gladney <jamezs@angelofcode.com>
2014-05-08 08:12:08 -0400

    new code

--- nickserv.js
+++ nickserv.js
@@ -1,9 +1,9 @@
 /* jslint node:true */
 /* https://toxin.jottit.com/unrealircd_services_commands */
-var configuration = require('./configuration.jso') || require('configuration-example.json');
+var configuration = require('./configuration.json') || require('configuration-example.json');
 var request = require('request');
 var irc = require('irc');
-var currentUsers = Object.create(null);
+var currentUsers = {};
 
 function userJoinsServer(username) {
     currentUsers[username] = false;
@@ -17,25 +17,43 @@ function userJoinsServer(username) {
 }
 
 function attemptLogin(username, password) {
+    if (currentUsers.hasOwnProperty(username)) {
+        request.post(configuration.loginURL, function loginResponse(error, httpResponse, body) {
+            if (httpResponse.statusCode == 304) {
+                currentUsers[username] = true;
+                //tell user they are logged in
+                //set user +r
+                ///kidnap user
+            }
+            console.log(httpResponse.statusCode, 'did it!', httpResponse.headers);
 
-    request.post(configuration.loginURL, function loginResponse(error, httpResponse, body) {
-        currentUsers[username] = true;
-        //tell user they are logged in
-        //set user +r
-        ///kidnap user
-    }).form({
-        log: username,
-        pwd: password,
-        'wp-submit': 'Log In',
-        redirect_to: configuration.redirect
-    });
+        }).form({
+            log: username,
+            pwd: password,
+            'wp-submit': 'Log In',
+            redirect_to: configuration.redirect
+        });
+
+
+    } else {
+        console.log('I didnt see you come into this server myfriend,...');
+    }
 }
 
-var ircbot = new irc.Client(configuration.ircURI, configuration.botname, {
-    channels: ['#ircops'],
-    debug: true
-});
+function loadIRC() {
+    var ircbot = new irc.Client(configuration.ircURI, configuration.botname, {
+        channels: ['#ircops'],
+        debug: true
+    });
 
-ircbot.addListener('message#ircops', function (nick, to, text, message) {
-    
-});
+    ircbot.addListener('notice', function (nick, to, text, message) {
+        if (message.server) {
+            if (message.args[2] === 'connecting') {
+
+            } else if (message.args[2] === 'exiting') {
+
+            }
+            console.log(message.args[5]);
+        }
+    });
+}
